fe7e2322f70ee13a84b403e2998cc018b106ed96
Leyan Lo <leyanlo@gmail.com>
2019-02-04 14:11:37 -0800

    Add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-config-prettier',
+  ],
+  rules: {
+    'length-zero-no-unit': [
+      true,
+      {
+        ignore: ['custom-properties'],
+      },
+    ],
+  },
+};
